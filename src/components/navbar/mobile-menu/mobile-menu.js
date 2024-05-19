a158761a4c0d20cfdbcbf112530251c9315f5361
nasr89 <nasrnasr8915@gmail.com>
2024-05-19 19:29:26 +0300

    finish navbar

--- src/components/navbar/mobile-menu/mobile-menu.js
+++ src/components/navbar/mobile-menu/mobile-menu.js
@@ -1,5 +1,34 @@
-function MobileMenu() {
-  return <div>Mobile Menu</div>;
+import { Link, useLocation } from "react-router-dom";
+import { isStoreSelected, isCartSelected } from "utils/checkRoutes";
+function MobileMenu({ closeFn }) {
+  const loc = useLocation();
+  return (
+    <div className="mobile-menu">
+      <div className="mobile-menu__content">
+        <Link
+          onClick={closeFn}
+          to="/"
+          className={`mobile-menu__content__item
+      ${
+        isStoreSelected(loc.pathname) && "mobile-menu__content__item--selected"
+      }`}
+        >
+          Store
+        </Link>
+        <Link
+          onClick={closeFn}
+          to="/cart"
+          className={`mobile-menu__content__item
+      ${
+        isCartSelected(loc.pathname) && "mobile-menu__content__item--selected"
+      }`}
+        >
+          Cart
+        </Link>
+        <button className="mobile-menu__content__btn primary">Login</button>
+      </div>
+    </div>
+  );
 }
 
 export default MobileMenu;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,22 +2,55 @@ import { Link } from "react-router-dom";
 import useWindowSize from "utils/useWindowSize";
 import MobileMenu from "./mobile-menu/mobile-menu";
 import DesktopMenu from "./desktop-menu/desktop-menu";
+import { useState, useEffect } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
 function Navbar() {
   const { width } = useWindowSize();
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const openMenu = () => {
+    setIsMenuOpened(true);
+  };
+  const closeMenu = () => {
+    setIsMenuOpened(false);
+  };
+
+  useEffect(() => {
+    if (width > 800) {
+      closeMenu();
+    }
+  });
   return (
-    <div className="navbar">
-      <div className="navbar__left-side">
-        <Link to="/">
-          <div className="navbar__left-side__logo">
-            <span className="navbar__left-side__logo__text">
-              DEVRABIC <b>ECOM</b> STORE
-            </span>
-          </div>
-        </Link>
+    <div>
+      <div className="navbar">
+        <div className="navbar__left-side">
+          <Link to="/">
+            <div className="navbar__left-side__logo">
+              <span className="navbar__left-side__logo__text">
+                DEVRABIC <b>ECOM</b> STORE
+              </span>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar__right-side">
+          {width < 800 ? (
+            isMenuOpened ? (
+              <AiOutlineClose
+                className="navbar__right-side__icon"
+                onClick={closeMenu}
+              />
+            ) : (
+              <RxHamburgerMenu
+                className="navbar__right-side__icon"
+                onClick={openMenu}
+              />
+            )
+          ) : (
+            <DesktopMenu />
+          )}
+        </div>
       </div>
-      <div className="navbar__right-side">
-        {width < 800 ? <MobileMenu /> : <DesktopMenu />}
-      </div>
+      {isMenuOpened && <MobileMenu closeFn={closeMenu} />}
     </div>
   );
 }
